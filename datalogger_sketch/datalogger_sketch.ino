884d539a478b97f6d539fa534d762b397355e45b
Bird <adam.bird@ontario.ca>
2020-07-09 08:12:51 -0400

    improved commenting

--- datalogger_sketch/datalogger_sketch.ino
+++ datalogger_sketch/datalogger_sketch.ino
@@ -1,23 +1,21 @@
 //Include required libraries
-#include <SD.h>
-#include <Wire.h>
-#include <SPI.h>
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BME680.h"
-#include "SoftwareSerial.h"
-#include "RTClib.h"
-#include <OneWire.h>
-#include <DallasTemperature.h>
+#include <SD.h> //Library for SD card Logger
+#include <Wire.h> //Library for I2C Interface
+#include <SPI.h> //Library for SPI interface
+#include <Adafruit_Sensor.h> //Common Library for Adafruit Sensors 
+#include "Adafruit_BME680.h" // Library for Combined Environmental Sensor BME680
+#include "SoftwareSerial.h" //Library for Configuring Serial Interface for T66 CO2 Sensor
+#include "RTClib.h" //Library for Real Time Clock RTC_PCF8523
+#include <OneWire.h> //Interface for DS18B20 Temperature Sensor
+#include <DallasTemperature.h> //Library for DS18B20 Temperature Sensor
 //----------------------------------------------------------------------
 
 //Set up UART connection for CO2 sensor
-SoftwareSerial T66_Serial(6,7); //Sets up a virtual serial port
- //Using pin 12 for Rx and pin 13 for Tx
+SoftwareSerial T66_Serial(6,7); //Sets up a virtual serial port on pin 6 for Rx and pin 7 for Tx
 byte readCO2[] = {0xFF, 0XFE,2,2,3}; //T66 read CO2 command: 5 bytes
-
 byte response[] = {0,0,0,0,0}; //create an array to store the response
 //---------------------------------------------------------------------
-//
+
 //Set up BME 680 sensor
 #define BME_SCK 13
 #define BME_MISO 12
@@ -25,29 +23,18 @@ byte response[] = {0,0,0,0,0}; //create an array to store the response
 #define BME_CS 10
 #define ONE_WIRE_BUS 8
 #define SEALEVELPRESSURE_HPA (1013.25)
-
-
-Adafruit_BME680 bme; // I2C
-//----------------------------------------------------------------------
-//Setup real time clock
-RTC_PCF8523 rtc; // define the Real Time Clock object
 //---------------------------------------------------------------------
-// for the data logging shield, we use digital pin 10 for the SD cs line
 
-const int pinCS = 10;
+// Misc Configuration for Buzzer, BME680, RTC, Dattalogger, DS18B20
+const int buzzer = 9; //Set up buzzer on arduino pin 9
+Adafruit_BME680 bme; //Set up BME680 I2C
+RTC_PCF8523 rtc; //Setup real time clock
+const int pinCS = 10; // Set up logging sheild 10 for the SD cs line
 OneWire oneWire(ONE_WIRE_BUS);
-
 DallasTemperature sensors(&oneWire);
-
- float Celcius=0;
-
-// the logging file
-
-
-//-----------------------------------------------------------------------
-
-
+float Celcius=0;
 //----------------------------------------------------------------------
+
 void setup()
 {
   // put your setup code here, to run once:
@@ -65,14 +52,18 @@ void setup()
   bme.setGasHeater(320, 150); // 320*C for 150 ms
   sensors.begin();
   sensors.setWaitForConversion(false);
+  pinMode(buzzer, OUTPUT); // Set buzzer - pin 9 as an output
 
   T66_Serial.begin(19200); //Opens the virtual serial port with a baud of 19200
 
   Serial.println(" Datalogger Start");
   Serial.print("\n");
 
-  
-delay(1000); //wait 10 monutes for sensors to warm up before entering the loop
+  delay(300000); //wait 5 minutes for CO2 sensor to warm up before entering the loop
+  tone(buzzer, 1000); // Send 1KHz sound signal...
+  delay(1000);        // ...for 1 sec
+  noTone(buzzer);     // Stop sound...
+  delay(1000);        // ...for 1sec  
 }
 
 void loop() {
